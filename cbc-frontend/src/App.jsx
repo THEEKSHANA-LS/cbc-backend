193f97a2b922b3611b3309a7b620d250aa915b49
THEEKSHANA L.S. <stheekshana90@gmail.com>
2025-12-06 01:48:05 +0530

    ui change for  mobile

--- cbc-frontend/src/App.jsx
+++ cbc-frontend/src/App.jsx
@@ -14,6 +14,13 @@ function App() {
   return (
     <BrowserRouter>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <div className="">
+      <div className="lg:hidden fixed inset-0 -z-10">
+        <div className="relative h-full w-full bg-black">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+          <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
+        </div>
+      </div>
        <div className="w-full h-[100vh]"> 
 
        <Toaster position="top-center"/>
@@ -27,7 +34,7 @@ function App() {
           <Route path="/account-settings" element={<AccountSettings/>}/>
           <Route path="/test" element={<TestPage/>}/>
         </Routes>
-
+       </div>
        </div>
       </GoogleOAuthProvider>;
     </BrowserRouter>
